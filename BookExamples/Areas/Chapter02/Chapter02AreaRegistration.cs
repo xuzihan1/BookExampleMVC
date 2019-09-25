1b1c85ccf05b89250d0f4b4f2773aa0ef445b4c3
xuzihan1 <917066882@qq.com>
2019-09-25 10:07:34 +0800

    第二章

---
+++ BookExamples/Areas/Chapter02/Chapter02AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace BookExamples.Areas.Chapter02
+{
+    public class Chapter02AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Chapter02";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Chapter02_default",
+                "Chapter02/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BookExamples/Areas/Chapter02/Controllers/ch02DemosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookExamples.Areas.Chapter02.Controllers
+{
+    public class ch02DemosController : Controller
+    {
+        // GET: Chapter02/ch02Demos
+        public ActionResult Index(string id)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(id);
+            }
+            else
+            {
+                return View(id);
+            }
+        }
+    }
+}
