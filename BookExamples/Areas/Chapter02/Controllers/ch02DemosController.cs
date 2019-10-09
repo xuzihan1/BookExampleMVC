120af51bbf9f430e223a9271d3b574758a50ba38
xuzihan1 <917066882@qq.com>
2019-10-09 09:37:52 +0800

    例2-2和例2-3

--- BookExamples/Areas/Chapter02/Controllers/ch02DemosController.cs
+++ BookExamples/Areas/Chapter02/Controllers/ch02DemosController.cs
@@ -20,5 +20,36 @@ namespace BookExamples.Areas.Chapter02.Controllers
                 return View(id);
             }
         }
+
+        public ActionResult Welcome()
+        {
+            var id = RouteData.Values["id"].ToString();
+            var name = Request["name"];
+            var age = Request["age"];
+            var url = Request.Url;
+            string s = "";
+            if (id=="1")
+            {
+                s = string.Format("欢迎，你的id为：{0}\n访问的URL为：{1}", id, url);
+
+            }
+            else if (id == "2")
+            {
+                s = string.Format(
+                    "欢迎你，{0}，\n访问的URL为：{1}\n\n" +
+                    "地址栏的“{2}”为自动对参数“{0}”进行URL编码后的结果。",
+                    name, url, Server.UrlEncode(name));
+            }
+            else
+            {
+                s = string.Format(
+                    "欢迎你，{0}岁的{1}，\n访问的URL为：{2}\n\n" +
+                    "地址栏的“{3}”为自动对参数“{1}”进行URL编码后的结果。",age,
+                    name, url, Server.UrlEncode(name));
+            }
+
+            ViewBag.Message = s;
+            return View();
+        }
     }
 }
